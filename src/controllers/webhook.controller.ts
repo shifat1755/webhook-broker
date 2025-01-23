0ee68aeeb7bfe5708943888e3885ed561a0b87c2
Syed Shifat <SyedShifat.Hasan@optimizely.com>
2025-01-24 01:39:04 +0600

    fetch_A_Webhook Api added

--- src/controllers/webhook.controller.ts
+++ src/controllers/webhook.controller.ts
@@ -1,16 +1,11 @@
-import { addWebhookToRedis } from "../services/webhook.service";
+import { addWebhookToRedis,getOneWebhookFromRedis } from "../services/webhook.service";
 import { Request, Response } from "express";
 
 
 export const addWebHook = async (req: Request, res: Response):Promise<any> => {
-  console.log("Request body:", req.body);
-
   try {
-    console.log("Request body:", req.body);
     await addWebhookToRedis(req.body.eventName, req.body.webhookUrl);
-    return res.status(200).json({
-      message: `WebhookUrl for event "${req.body.eventName}" added successfully!`,
-    });
+    return res.status(200).json(req.body);
   } catch (err) {
     console.error("Error adding webhook:", err);
     return res.status(500).json({
@@ -20,11 +15,23 @@ export const addWebHook = async (req: Request, res: Response):Promise<any> => {
 }
 
 export const getWebHooks = async (req: Request, res: Response):Promise<any> => {
-return null;
+  // try{
+  //   const response=await
+  // }
+  return null;
 }
 export const triggerEvent = async (req: Request, res: Response):Promise<any> => {
   return null;
 }
+
 export const getSpecificWebHook = async (req: Request, res: Response):Promise<any> => {
-  return null;
+  try{
+    const response=await getOneWebhookFromRedis(req.params.eventName);
+    return res.status(200).json(response);
+  }catch(err){
+    console.error("Error getting webhook:", err);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 }
