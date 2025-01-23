2c75b7dd0e73a02991dc9e12f3d2cd5a9da40734
Syed Shifat <SyedShifat.Hasan@optimizely.com>
2025-01-24 00:29:34 +0600

    feat: added webhook post req to redis

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import router from "./routes/webhook.route";
 import "./config/redisClient.config";
 dotenv.config();
 
 
 const app=express();
 const PORT=process.env.PORT;
+app.use(express.json());
+app.use('',router)
 
 app.get("/",(req,res)=>{
     res.status(200).send("up and running like a Bee...");
--- src/services/webhook.service.ts
+++ src/services/webhook.service.ts
@@ -1,3 +1,14 @@
+import { redisClient } from "../config/redisClient.config";
 export const triggerEvent = async (event: string) => {
     return null;
 }
+
+export const addWebhookToRedis = async (eventName: string, webHookUrl: string) => {
+    try {
+      const key = `event:${eventName}`;
+        await redisClient.rpush(key, webHookUrl);
+      console.log(`Webhooks for event "${eventName}" added successfully!`);
+    } catch (err) {
+      console.error(`Error adding webhooks for event "${eventName}":`, err);
+    }
+  };
